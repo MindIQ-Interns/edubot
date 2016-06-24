a849bf90f8eb46247e0a896e787983f7e3f6d319
Nitin John <nitinjohnraj@gmail.com>
2016-06-24 13:47:45 +0530

    Added MessengerView with functionality to receive messages

--- messenger_interface/urls.py
+++ messenger_interface/urls.py
@@ -1,3 +1,6 @@
 from django.conf.urls import url
+from . import views
 
-urlpatterns = []
+urlpatterns = [
+    url(r'^0802c8786a09ae44a56a072d9d9a5e5da3172747df3d39e915/', views.MessengerView.as_view(), name='messenger_view'),
+]
