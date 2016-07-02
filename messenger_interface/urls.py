ba390f0c3561b11846695919f6cb89763f197cc7
Nitin John <nitinjohnraj@gmail.com>
2016-07-02 13:58:01 +0530

    Deleted quiz_bot and messenger_interface, to recreate them with REST framework.

--- messenger_interface/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^0802c8786a09ae44a56a072d9d9a5e5da3172747df3d39e915/', views.MessengerView.as_view(), name='messenger_view'),
-]
